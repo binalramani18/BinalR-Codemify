b96ce732fc98937ad8d5b8a8d8fc9832e9a0c3e5
binalramani18 <binalramani@yahoo.com>
2024-11-17 13:57:14 -0500

    removedextracomments

--- week-6-JS-Challenge/helper.methods.js
+++ week-6-JS-Challenge/helper.methods.js
@@ -2,11 +2,6 @@ class Helpers {
     filterMatchesByLocationAndStatus(response) {
         
 
-        // Challenge 1: Filter Matches by Location and Status
-        // Write a function to find all matches where "locationlvl1" = "Germany" and "status" = "canceled" in the array.
-        // The function should return an array of "matchId"s of the matches.
-        // Example usage:
-        // console.log(filterMatchesByLocationAndStatus(response); // Returns: ["10-016", "10-015"]
         const result = [];
         response.matches.forEach(element => {
             
@@ -20,15 +15,7 @@ class Helpers {
         return result;
     }
 
-    // Challenge 2: Filter Matches by Coating and Shower Availability
-    // Write a function to find all matches where "coating" = "Main.artificialGrass" and "isShower" = true in the array.
-    // The function should return an array of "matchId"s of the matches.
-    // Example usage:
-    // console.log(filterMatchesByCoatingAndShower(response)); // Returns: ["10-015"]
     
-      //  response.matches.forEach(element => {    |<----here we log each element to see it's structure
-        //    console.log(element);                | 
-        //});                                      | 
 
     filterMatchesByCoatingAndShower(response) {
         
@@ -49,13 +36,7 @@ class Helpers {
     }
 
     filterMatchesByLocationAndPrice(response) {
-        
-       // Challenge 3: Filter Matches by Location and Price
-        // Write a function to find all matches where "locationlvl2" = "Berlin" and "price" > 0 in the array.
-        // The function should return an array of "matchId"s of the matches.
-        // Example usage:
-        // console.log(filterMatchesByLocationAndPrice(response)); // Returns: ["10-015"]
-
+     
         const locationAndPrice = [];
         response.matches.forEach(element => {
             if (element.locationlvl2 === "Berlin" && element.price > 0) {
@@ -67,12 +48,7 @@ class Helpers {
         return locationAndPrice;
     }
 
-    // Challenge 4: Filter Matches by Creator Fee and Status
-    // Write a function to find all matches where "creatorFeePerPlayer" > 3 and "status" = "canceled" in the array.
-    // The function should return an array of "matchId"s of the matches.
-    // Example usage:
-    // console.log(filterMatchesByCreatorFeeAndStatus(response)); // Returns: ["10-016", "10-015"]
-    
+   
     
     filterMatchesByCreatorFeeAndStatus(response) {
         
@@ -87,11 +63,8 @@ class Helpers {
 
         return result;
     }
-        // Challenge 5: Filter Matches by Covering and Dressing Room Availability
-        // Write a function to find all matches where "covering" = "Main.onTheStreet" and "isDressingRoom" = true in the array.
-        // The function should return an array of "matchId"s of the matches.
-        // Example usage:
-        // console.log(filterMatchesByCoveringAndDressingRoom(response)); // Returns: ["10-016"]
+
+    
     filterMatchesByCoveringAndDressingRoom(response) {
         
         let result =[];
